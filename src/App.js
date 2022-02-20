234fd932f0c202ab33d675d15902f8eec1ed6d3d
Jarvis <jarvismiranda@gmail.com>
2022-02-20 15:53:28 -0500

    subiendo pagina principal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import Imagen from "./img/breakingbad.svg.png";
+
+const Boton = styled.button`
+  margin-top: 5rem;
+  padding: 1rem 2.5rem;
+  color: white;
+  background-color: black;
+  font-weight: bold;
+  font-size: 20px;
+  border: none;
+  text-transform: uppercase;
+  border-radius: 10px;
+`;
+
+const Frase = styled.div`
+  padding: 2rem;
+  font-size: 50px;
+  font-family: "Dancing Script";
+  color: white;
+`;
+
+const Autor = styled.div`
+  text-align: right;
+  margin-right: 4rem;
+  color: white;
+  font-family: "Dancing Script";
+  font-size: 25px;
+`;
+
+const Contenedor = styled.div`
+  margin-top: 4rem;
+  text-align: center;
+  padding: 2rem;
+`;
+
+const IMG = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
 
 function App() {
+  //hooks
+  const [frase, setFrase] = useState("");
+  const [autor, setAutor] = useState("");
+
+  useEffect(() => {
+    handleSubmit();
+  }, []);
+
+  const handleSubmit = async () => {
+    const url = "https://breakingbadapi.com/api/quote/random";
+    const resultado = await fetch(url);
+    const respuesta = await resultado.json();
+    const { quote, author } = respuesta[0];
+    setFrase(quote);
+    setAutor(author);
+    console.log(respuesta);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Contenedor>
+        <IMG src={Imagen}></IMG>
+        <Frase>"{frase}"</Frase>
+        <Autor>{autor}</Autor>
+        <Boton onClick={handleSubmit}>Obtener Frase</Boton>
+      </Contenedor>
+    </>
   );
 }
 
